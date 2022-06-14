6fc7fa6ccab9f03ef57c5b0676cf2d4ac5becf3e
Jordan <jazoulay@joazco.com>
2022-06-14 11:14:08 +0200

    prepare multiple task

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,27 @@
 import React, { useState, FC } from "react";
 import TableComponent from "./components/TableComponents";
 import FormComponent from "./components/FormComponents";
-import { TaskType } from "./types"
-
+import { TaskType } from "./types";
 
 const App: FC = () => {
-  const [task, setTask] = useState<TaskType>();
-  console.log(task, 'task')
-  console.log(setTask, 'setTask')
+  const [tasks, setTasks] = useState<TaskType[]>([]);
 
+  const addTask = (t: TaskType) => {
+    // setTasks(Array.from(tasks.concat(t)));
+
+    setTasks((_tasks: TaskType[]) => {
+      _tasks = _tasks.concat(t);
+      return Array.from(_tasks);
+    });
+  };
+
+  const deleteTask = (t: TaskType) => {
+    // _tasks.filter((_t: TaskType) => {
+    //   if (_t.content === t.content) return true;
+    //   return false;
+    // });
+    // _tasks.filter((_t: TaskType) => _t.content === t.content)
+  };
 
   return (
     <>
@@ -16,10 +29,10 @@ const App: FC = () => {
         <h1>Todo List</h1>
       </div>
       <div>
-        <FormComponent onSubmit={setTask} />
+        <FormComponent onSubmit={addTask} />
       </div>
       <div>
-        <TableComponent task={task} />
+        <TableComponent tasks={tasks} onDelete={deleteTask} />
       </div>
     </>
   );
--- src/components/TableComponents/index.tsx
+++ src/components/TableComponents/index.tsx
@@ -1,43 +1,26 @@
-import TaskComponent from "../TaskComponents";
-import { useState } from "react";
 import { TaskType } from "../../types";
 
 type TableComponentProps = {
-  task: TaskType | undefined,
+  tasks: TaskType[];
+  onDelete: (task: TaskType) => void;
 };
 
 const TableComponent = (props: TableComponentProps) => {
-  const { task } = props;
-  console.log(task, 'olé')
-  const [tasks, setTasks] = useState<TaskType[]>([]);
-  // const [checkTask, setCheckTask] = useState<boolean>();
-  // const [deleteTask, setDeleteTask] = useState<boolean>();
-
-  const addTask = (): void => {
-    setTasks([...tasks, task]);
-  };
-
-  // const handleDeleteTask = (content) =>{
-  //   setTasks(tasks.filter((task)=> task.content !== content))
-  // }
-
-  // const handleCompleteTask = (task) =>{
-  //   setTasks(
-  //     tasks.map((item) => {
-  //       if (item.content === task.content){
-  //         return{...item, completed: !item.completed}}})}
-
-
+  const { tasks, onDelete } = props;
 
   return (
     <ul>
       {tasks.map((task: TaskType, i: number) => {
         return (
-          <li>
-            <TaskComponent key={i} task={task} 
-              // checkTask={checkTask}
-              // deleteTask={deleteTask}
-            />
+          <li key={`table-component-${i}`}>
+            {task.content}{" "}
+            <button
+              onClick={() => {
+                onDelete(task);
+              }}
+            >
+              X
+            </button>
           </li>
         );
       })}
